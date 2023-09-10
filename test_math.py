08a1585b54b7a6dc1e136b226019ec234dab5f28
redditisha <117503669+redditisha@users.noreply.github.com>
2023-09-10 15:23:46 +0530

    Update test_math.py

--- test_math.py
+++ test_math.py
@@ -1,12 +1,15 @@
 # Import math Library
 import math
 from selenium import webdriver
+from selenium.webdriver.chrome.options import ChromeOptions
 
 # Return the base-10 logarithm of different numbers
 print(math.log10(2.7183))
 print(math.log10(2))
 print(math.log10(1))
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(options=chrome_options)
 driver.maximize_window()
 
 driver.get("https://trends.google.com")
